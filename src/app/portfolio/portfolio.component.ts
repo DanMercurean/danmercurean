2715d532cf95cbd596354c362969b69ba2fa78a4
Dan Mercurean <danymercurean@yahoo.com>
2021-03-03 19:25:24 +0100

    portfolio update

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,112 +1,4 @@
-// import {
-//   AfterContentInit,
-//   AfterViewChecked,
-//   AfterViewInit,
-//   Component,
-//   ElementRef,
-//   HostListener,
-//   OnInit,
-//   ViewChild,
-// } from '@angular/core';
-// import { SlideInAnimation } from '../animations';
-// import { AnimationTriggerService } from '../services/animationtriggerservice';
-// import { projects } from '../../assets/data/projects';
-
-// @Component({
-//   selector: 'app-portfolio',
-//   templateUrl: './portfolio.component.html',
-//   styleUrls: ['./portfolio.component.scss'],
-//   animations: [SlideInAnimation],
-// })
-// export class PortfolioComponent implements OnInit, AfterViewChecked {
-//   animationState = this.triggerService.animationState;
-//   currentFilter = '';
-//   projects = projects;
-//   visibleProjects = this.projects;
-//   columns: number;
-
-//   constructor(public triggerService: AnimationTriggerService) {}
-
-//   ngOnInit(): void {
-//     this.arrangeGrid();
-//   }
-
-//   ngAfterViewChecked() {
-//     this.triggerService.pagePosition$.subscribe(() => {
-//       this.updateOffset();
-//     });
-//   }
-
-//   /**
-//    * Adapts no. of columns per grid-row based on screen-width
-//    */
-//   @HostListener('window:resize')
-//   arrangeGrid() {
-//     let screenWidth = window.innerWidth;
-
-//     if (screenWidth >= 1300) {
-//       this.columns = 3;
-//     }
-//     if (screenWidth < 1300) {
-//       this.columns = 2;
-//     }
-//     if (screenWidth < 850) {
-//       this.columns = 1;
-//     }
-
-//     return this.columns;
-//   }
-
-//   // Access Elements to detect offset to document top
-//   @ViewChild('portfolioHeader') portfolioHeader: ElementRef;
-//   @ViewChild('portfolioFilter') portfolioFilter: ElementRef;
-//   @ViewChild('portfolioGrid') portfolioGrid: ElementRef;
-
-//   headerOffset = 0;
-//   filterOffset = 0;
-//   portfolioOffset = 0;
-//   elementOffsetTop = this.triggerService.elementOffsetTop;
-
-//   updateOffset() {
-//     this.arrangeGrid();
-//     let pageOffsetY = this.triggerService.currentPagePosition;
-//     const rectHeader = this.portfolioHeader.nativeElement.getBoundingClientRect();
-//     const rectFilter = this.portfolioFilter.nativeElement.getBoundingClientRect();
-//     const rectPortfolio = this.portfolioGrid.nativeElement.getBoundingClientRect();
-
-//     // Add element's offset to viewport-top to the offset already scrolled (pageYOffset)
-//     this.headerOffset = rectHeader.top + pageOffsetY;
-//     this.filterOffset = rectFilter.top + pageOffsetY;
-//     this.portfolioOffset = rectPortfolio.top + pageOffsetY;
-//     // Update offset-object from triggerService
-//     this.elementOffsetTop.portfolio_header = this.headerOffset;
-//     this.elementOffsetTop.portfolio_filter = this.filterOffset;
-//     this.elementOffsetTop.portfolio = this.portfolioOffset;
-//   }
-
-//   /**
-//    * Filter visible projects
-//    * @param newFilter: defines current filter selection for visible project-portfolio
-//    */
-//   updateFilter(newFilter?: string) {
-//     if (newFilter == undefined) {
-//       this.currentFilter = '';
-//       this.visibleProjects = this.projects;
-//     } else if (newFilter == 'Angular') {
-//       this.currentFilter = 'Angular';
-//       this.visibleProjects = this.projects.filter(
-//         (project) => project.category == 'Angular'
-//       );
-//     } else if (newFilter == 'JavaScript') {
-//       this.currentFilter = 'JavaScript';
-//       this.visibleProjects = this.projects.filter(
-//         (project) => project.category == 'JavaScript'
-//       );
-//     }
-//   }
-// }
-
-import { Component, OnInit } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
@@ -137,7 +29,7 @@ filteredProjects = [];
   ngOnInit(): void {
   }
 
-  show(filter){
+  show( filter ){
     this.filteredProjects = this.getFiltered(filter);
     // console.log(filteredProjects);
     if(this.filteredProjects.length == 0)
@@ -146,12 +38,12 @@ filteredProjects = [];
       this.populateProjectList(this.filteredProjects);
     }
 }
-populateProjectList( array ){
+populateProjectList( allProjects ){
   this.projectsList.innerHTML = '';
-  for(let i = 0; i < array.length; i++){
+  for(let i = 0; i < allProjects.length; i++){
       this.projectsList.innerHTML += `
-          <div class="projects">
-              <p>${array[i].name}</p>
+          <div class="projects-list">
+              <p>${allProjects[i].name}</p>
           </div>
       `;
   }
